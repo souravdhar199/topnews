610017ea61d95a4a35d1a5f55da3836dc6994c55
sourav199 <souravdhar1999@gmail.com>
2022-09-12 21:48:55 -0400

    App Creation done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import FetchDataSports from './FetchDataSports';
 
 function App() {
+  const [lookup, Setlookup] = useState('');
+  const [lookup2, Setlookup2] = useState('');
+
+  function handelSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.name.value)
+    Setlookup2(lookup);
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handelSubmit}>
+        <h1>Welcome... Type anything to find out whats going on </h1>
+        <label>
+          <input className="input1" onChange={(e) => Setlookup(e.target.value)} type="text" name='name' value={lookup} />
+        </label>
+        <input className="input2" type="submit" value="Search" />
+      </form>
+
+      {/* This component will show all the news.  */}
+      <FetchDataSports value={lookup2} />
     </div>
   );
 }
---
+++ src/FetchDataSports.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import './App.css';
+function FetchDataSports({ value }) {
+    const [sports, Setsports] = useState([]);
+    //This will fetch data from the NewYork times API
+    console.log(process.env)
+    useEffect(() => {
+        fetch(`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${value}&api-key=${process.env.REACT_APP_API_KEY}`)
+            .then(response => response.json())
+            .then(data => Setsports([...data.response.docs]))
+            .catch(error => console.log(error));
+
+    }, [value]); // [value] --> Refresh again if the state updates
+
+    return (
+        <div className="parent">
+            {sports.map((item) => (
+                <div className="children">
+                    <h1>{item.headline.main}</h1>
+                    <p>{item.lead_paragraph}</p>
+                    <button><a href={item.web_url}> See the News</a></button>
+                </div>
+            ))}
+
+        </div>
+
+    );
+}
+
+
+export default FetchDataSports
