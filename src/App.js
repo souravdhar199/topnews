ec969685960e9bfd6eb8561a55f96ed5c5f0ebf6
sourav199 <souravdhar1999@gmail.com>
2022-09-12 22:35:56 -0400

    Editted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import FetchDataSports from './FetchDataSports';
 
 function App() {
